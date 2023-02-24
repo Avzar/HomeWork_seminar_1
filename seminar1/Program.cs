3e0a6153411b1080bbef3733bcdc0d2ca8a8ab9d
Avzar <rassstinson@gmail.com>
2023-02-24 19:59:20 +0600

    seminar1 added

---
+++ seminar1/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Enter two numbers");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    if (number1 / number2 == number2)
+    {
+        Console.WriteLine("Число являектся квадратом");
+    }
+    else
+    {
+        Console.WriteLine("Число не являектся квадратом");
+    }
+
+}
+else
+{
+    if (number2 / number1 == number1)
+    {
+        Console.WriteLine("Число являектся квадратом");
+    }
+    else
+    {
+        Console.WriteLine("Число не являектся квадратом");
+    }
+
+}
