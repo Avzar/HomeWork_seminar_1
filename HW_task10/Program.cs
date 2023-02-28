975552d66e9f83c2c97fb16c7a6972cd7c89b372
Avzar <rassstinson@gmail.com>
2023-02-28 12:31:06 +0600

    task 10 completed and added

---
+++ HW_task10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int number = Random.Shared.Next(100,1000);
+Console.WriteLine(number);
+int firstdigit = number; 
+int middledigit = number / 10;
+int lastdigit = number;
+int result = middledigit % 10;
+Console.WriteLine(result);
