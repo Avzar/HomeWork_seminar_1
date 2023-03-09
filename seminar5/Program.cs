392a2ccf79dd509211f901797f6f4657619ebf78
Avzar <rassstinson@gmail.com>
2023-03-10 01:20:21 +0600

    Seminar 4 tasks added (task 32)

---
+++ seminar5/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] GetRandomArray()
+{
+    int[] array = new int[12];
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = Random.Shared.Next(-9, 10);
+    }
+
+    return array;
+}
+
+int SumNegativeElements(int[ array])
+{
+    int sum = 0;
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        sum += array[i];
+    }
+}
