60b20ce0860dc7b55afcd4fc76a4606704117fe5
Avzar <rassstinson@gmail.com>
2023-03-17 17:11:16 +0600

    tasks41, 43 added and completed

--- HW_task41/Program.cs
+++ HW_task41/Program.cs
@@ -4,7 +4,7 @@
 
 // 1, -7, 567, 89, 223-> 3
 
-System.Console.WriteLine("Insert numbers");
+Console.Write("Введите числа через запятую: ");
 int[] numbers = StringToNum(Console.ReadLine());
 PrintArray(numbers);
 int sum = 0;
@@ -15,16 +15,54 @@ for (int i = 0; i < numbers.Length; i++)
         sum++;
     }
 }
-System.Console.WriteLine();
-System.Console.WriteLine($"count of numbers > 0 = {sum}");
+Console.WriteLine();
+Console.WriteLine($"количество значений больше 0 = {sum}");
 
-int[] StringToNum(string input);
+
+int[] StringToNum(string input)
 {
     int count = 1;
-    for(int i = 0; i < input.Length; i++)
+    for (int i = 0; i < input.Length; i++)
     {
-        
+        if (input[i] == ',')
+        {
+            count++;
+        }
     }
+
+    int[] numbers = new int [count];
+    int index = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = "";
+
+        while (input [i] != ',')
+        {
+        if(i != input.Length - 1)
+        {
+            temp += input [i].ToString();
+            i++;
+        }
+        else
+        {
+            temp += input [i].ToString();
+            break;
+        }
+        }
+        numbers[index] = Convert.ToInt32(temp);
+        index++;
+    }
+    return numbers;
 }
 
-void PrintArray(int[] array);
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+}
